6f6967dfe75294a4d61cf9dd09e468b1e0742e7c
Daigo Kawasaki <kawasaki@fujisan.co.jp>
2020-12-23 19:45:31 +0900

    RunTaskConfigに一通りのECS引数を追記

--- src/main/scala/io/digdag/plugin/aws/ecs/runtask/RunTaskConfig.scala
+++ src/main/scala/io/digdag/plugin/aws/ecs/runtask/RunTaskConfig.scala
@@ -1,22 +1,106 @@
 package io.digdag.plugin.aws.ecs.runtask
 
 case class RunTaskConfig(
-  val cluster: String,
+  // val capacity_provider_strategy: Option[List[RunTaskConfig.CapacityProviderStrategy]],
+  val cluster: Option[String],
+  // val count: Option[Int],
+  val enable_ecs_managed_tags: Option[Boolean],
+  val group: Option[String],
+  val launch_type: Option[String],
+  val network_configuration: Option[RunTaskConfig.NetworkConfiguration],
+  val overrides: Option[RunTaskConfig.Override],
+  // val placement_constraints: Option[List[RunTaskConfig.PlacementConstraint]],
+  // val placement_strategy: Option[List[RunTaskConfig.PlacementStrategy]],
+  val platform_version: Option[String],
+  // val propagate_tags: Option[String],
+  // val reference_id: Option[String],
+  // val started_by: Option[String],
+  val tags: Option[List[RunTaskConfig.Tag]],
   val task_definition: String,
-  val launch_type: String,
-  val network_configuration: RunTaskConfig.NetworkConfiguration
 )
 
 object RunTaskConfig {
-  case class NetworkConfiguration(
-    val awsvpc_configuration: AwsVpcConfiguration
+
+  // CapacityProviderStrategy
+
+  case class CapacityProviderStrategy(
+    val capacityProvider: String,
+    val weight: Option[Integer],
+    val base: Option[Integer]
   )
+
+  // NetworkConfiguration
+
+  case class NetworkConfiguration(
+    val awsvpc_configuration: Option[AwsVpcConfiguration]
+  )
+
   case class AwsVpcConfiguration(
     val subnets: List[String],
-    val security_groups: List[String],
-    val assign_public_ip: String
+    val security_groups: Option[List[String]],
+    val assign_public_ip: Option[String]
+  )
+
+  // Override
+
+  case class Override(
+    val containerOverrides: Option[List[ContainerOverride]],
+    val cpu: Option[String],
+    val inference_accelerator_overrides: Option[List[InferenceAcceleratorOverride]],
+    val execution_role_arn: Option[String],
+    val memory: Option[String],
+    val task_role_arn: Option[String],
+  )
+
+  case class ContainerOverride(
+    val name: Option[String],
+    val command: Option[List[String]],
+    val environment: Option[List[Environment]],
+    val environment_files: Option[List[EnvironmentFile]],
+    val cpu: Option[Integer],
+    val memory: Option[Integer],
+    val memory_reservation: Option[Integer],
+    val resource_requirements: Option[List[ResourceRequirement]],
+  )
+
+  case class Environment(
+    val name: String,
+    val value: String,
+  )
+
+  case class EnvironmentFile(
+    val value: String,
+    val `type`: String,
+  )
+
+  case class ResourceRequirement(
+    val value: String,
+    val `type`: String,
+  )
+
+  case class InferenceAcceleratorOverride(
+    val device_name: Option[String],
+    val device_type: Option[String],
+  )
+
+  // PlacementConstraint
+
+  case class PlacementConstraint(
+    val `type`: String,
+    val expression: String,
+  )
+
+  // PlacementStrategy
+
+  case class PlacementStrategy(
+    val `type`: String,
+    val field: String,
+  )
+
+  // Tag
+
+  case class Tag(
+    val key: String,
+    val value: String,
   )
 }
-
-
-
--- src/main/scala/io/digdag/plugin/aws/ecs/runtask/RunTaskParams.scala
+++ src/main/scala/io/digdag/plugin/aws/ecs/runtask/RunTaskParams.scala
@@ -1,13 +1,14 @@
 package io.digdag.plugin.aws.ecs.runtask
 
 import scala.util.{Try,Failure}
+import collection.JavaConverters._
 import cats.implicits._
 import io.digdag.plugin.aws.ecs.implicits._
-import software.amazon.awssdk.services.ecs.{
-  EcsClient => SdkEcsClient
+import software.amazon.awssdk.services.ecs.model.{
+  RunTaskRequest,
+  NetworkConfiguration,
+  AwsVpcConfiguration,
 }
-
-import software.amazon.awssdk.services.ecs.model.RunTaskRequest
 import io.digdag.client.config.Config
 import io.circe.parser.parse
 import io.circe.generic.auto._
@@ -34,5 +35,32 @@ object RunTaskParams {
       request <- runTaskRequestBuild(config).toEither(Error.RunTaskRequestBuildErr)
     } yield RunTaskParams(request)
 
-  def runTaskRequestBuild(config: RunTaskConfig): Try[RunTaskRequest] = Failure(new RuntimeException(""))
+  // def runTaskRequestBuild(config: RunTaskConfig): Try[RunTaskRequest] = Failure(new RuntimeException(""))
+  def runTaskRequestBuild(config: RunTaskConfig): Try[RunTaskRequest] = Try{
+
+
+    // RunTaskRequest.builder()
+    //   .cluster(config.cluster)
+    //   .taskDefinition(config.task_definition)
+    //   .build()
+  }
+
+  def networkConfigurationBuild(opt: Option[RunTaskConfig.NetworkConfiguration]): Try[Option[NetworkConfiguration]] = Try{
+    // for (config <- opt)
+    //   awsvpcConfiguration <- awsvpcConfigurationBuild(config.awsvpc_configuration)
+    // } yield {
+    //   NetworkConfiguration.builder()
+    //     .awsvpcConfiguration()
+    // }
+  }
+
+  def awsvpcConfigurationBuild(opt: Option[RunTaskConfig.AwsVpcConfiguration]): Try[Option[AwsVpcConfiguration]] = Try{
+    for (config <- opt) yield {
+      AwsVpcConfiguration.builder()
+        .subnets(config.subnets.asJava)
+        .also(builder => config.security_groups.map(builder.securityGroups(_.asJava)))
+        .also(builder => config.assign_public_ip.map(builder.assignPublicIp(_)))
+        .build()
+    }
+  }
 }
